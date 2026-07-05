1fdc4f051713c23aa873f3242629f20f2e4a6e99
NazarACC <zablocky.n@gmail.com>
2026-07-05 23:01:10 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'tramper-pay-v1';
+const FILES = [
+  '/tramper-pay/',
+  '/tramper-pay/index.html',
+  '/tramper-pay/manifest.json',
+  '/tramper-pay/icon-192.png',
+  '/tramper-pay/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
